9434faf28d4b79af9b0b1a584dd347a5be9baf79
Tom PRESTON <t.preston@nodya-group.com>
2021-09-27 13:50:04 +0200

    fixed tests not found and added test case

--- test/test_views.py
+++ test/test_views.py
@@ -1,3 +1,4 @@
+from common.models import Person
 from django.test import TestCase
 from django.urls import reverse
 
@@ -11,4 +12,10 @@ class TestViews(TestCase):
     def test_index_view(self):
         r = self.client.get(self.url)
         self.assertEqual(r.status_code, 200)
-        self.assertEqual(r.content, b'Hello, world!')
+        self.assertEqual(r.content, b'I got no mates.')
+
+    def test_index_view_with_mates(self):
+        Person.objects.create(name='Bruce')
+        r = self.client.get(self.url)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.content, b'My friends are: Bruce')
